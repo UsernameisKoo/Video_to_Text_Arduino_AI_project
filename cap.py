1b90f9ba978053fae077178b77a00d38e2b37dee
UsernameisKoo <149701913+UsernameisKoo@users.noreply.github.com>
2024-05-30 14:26:18 +0900

    Add files via upload
    Recognizing Car license plate from Video

---
+++ cap.py
@@ -0,0 +1,44 @@
+#라이브러리 호출
+import cv2
+import os
+
+print(cv2.__version__)
+
+filepath = '1.mp4'
+video = cv2.VideoCapture(filepath) #'' 사이에 사용할 비디오 파일의 경로 및 이름을 넣어주도록 함
+
+if not video.isOpened():
+    print("Could not Open :", filepath)
+    exit(0)
+
+#불러온 비디오 파일의 정보 출력
+length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = video.get(cv2.CAP_PROP_FPS)
+
+print("length :", length)
+print("width :", width)
+print("height :", height)
+print("fps :", fps)
+
+#프레임을 저장할 디렉토리를 생성
+try:
+    if not os.path.exists(filepath[:-4]):
+        os.makedirs(filepath[:-4])
+except OSError:
+    print ('Error: Creating directory. ' +  filepath[:-4])
+
+count = 0
+
+while (video.isOpened()):
+    ret, image = video.read()
+    if (int(video.get(1)) % fps == 0):  # 앞서 불러온 fps 값을 사용하여 1초마다 추출
+        cv2.imwrite(filepath[:-4] + "/frame%d.jpg" % count, image)
+        print('Saved frame number :', str(int(video.get(1))))
+        count += 1
+
+video.release()
+
+
+
---
+++ video_to_text.py
@@ -0,0 +1,54 @@
+import easyocr
+import cv2
+import matplotlib.pyplot as plt
+import os
+THRESHOLD = 0.5
+
+reader = easyocr.Reader(['ko', 'en'])
+
+
+
+def get_image_files(directory):
+    image_files = []
+    for filename in os.listdir(directory):
+        # 파일의 확장자를 가져옴
+        _, extension = os.path.splitext(filename)
+        # 이미지 파일인지 확인
+        if extension.lower() in ['.jpg', '.jpeg', '.png', '.gif', '.bmp']:
+            image_files.append(os.path.join(directory, filename))
+    return image_files
+
+# 이미지 파일이 들어있는 디렉토리 경로
+directory_path = "1"
+image_files_list = get_image_files(directory_path)
+print(image_files_list)
+
+
+def read(image_files_list):
+
+    r = []
+    for img_path in image_files_list:
+
+        img = cv2.imread(img_path)
+
+        result = reader.readtext(img_path)
+
+
+
+        for bbox, text, conf in result:
+            if conf > THRESHOLD:
+                r.append(text)
+                cv2.rectangle(img, pt1=(int(bbox[0][0]), int(bbox[0][1])), pt2=(int(bbox[2][0]), int(bbox[2][1])), color=(0, 255, 0), thickness=3)
+
+            if len(r) > 1 and r[len(r)-2] == r[len(r)-1]:
+                pass
+                print(r[int(len(r))-1])
+        if len(r) > 1 and r[len(r) - 2] == r[len(r) - 1]:
+            pass
+            plt.figure(figsize=(8, 8))
+            plt.imshow(img[:, :, ::-1])
+            plt.axis('off')
+            plt.show()
+
+
+read(image_files_list)
