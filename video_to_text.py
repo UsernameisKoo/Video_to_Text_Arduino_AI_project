b2a26f2a3f6b17aaf9131a3971d1a22cb2c1654a
UsernameisKoo <syonkoo@naver.com>
2024-06-03 00:08:47 +0900

    Debugging duplicate license plate recognition

--- video_to_text.py
+++ video_to_text.py
@@ -26,31 +26,26 @@ image_files_list = get_image_files(directory_path)
 def read(image_files_list):
 
     r = []
-    for img_path in image_files_list:
 
+    for img_path in image_files_list:
+        str = ""
         img = cv2.imread(img_path)
 
         result = reader.readtext(img_path)
 
-
-
         for bbox, text, conf in result:
             if conf > THRESHOLD:
-                r.append(text)
+                str = str + " " + text
                 cv2.rectangle(img, pt1=(int(bbox[0][0]), int(bbox[0][1])), pt2=(int(bbox[2][0]), int(bbox[2][1])), color=(0, 255, 0), thickness=3)
 
-            if len(r) > 1 and r[len(r)-2] == r[len(r)-1]:
-                pass
-            else:
-                print(r[int(len(r))-1], end = " ")
-        print()
+        r.append(str)
         if len(r) > 1 and r[len(r) - 2] == r[len(r) - 1]:
             pass
         else:
+            print(r[int(len(r)) - 1])
             plt.figure(figsize=(8, 8))
             plt.imshow(img[:, :, ::-1])
             plt.axis('off')
             plt.show()
-
 
 read(image_files_list)
