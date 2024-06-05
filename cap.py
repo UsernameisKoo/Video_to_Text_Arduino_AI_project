c1c0964f9a92e23520983f74f6ad43aa01ddb8e4
UsernameisKoo <syonkoo@naver.com>
2024-06-05 22:42:15 +0900

    Add Comments

--- cap.py
+++ cap.py
@@ -1,25 +1,21 @@
-#라이브러리 호출
+# 라이브러리 호출
 import cv2
 import os
 
 print(cv2.__version__)
 
+# 영상 경로
 filepath = 'eng_ver.mp4'
-video = cv2.VideoCapture(os.path.join(os.path.dirname(os.path.abspath(__file__)), filepath)) #'' 사이에 사용할 비디오 파일의 경로 및 이름을 넣어주도록 함
+video = cv2.VideoCapture(os.path.join(os.path.dirname(os.path.abspath(__file__)), filepath)) # 상대 경로로 영상 경로 설정
 
+# 영상 초기화 실패 시 알려주고 종료
 if not video.isOpened():
     print("Could not Open :", filepath)
     exit(0)
 
 #불러온 비디오 파일의 정보 출력
-length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = video.get(cv2.CAP_PROP_FPS)
+fps = video.get(cv2.CAP_PROP_FPS) # 초당 프레임 수
 
-print("length :", length)
-print("width :", width)
-print("height :", height)
 print("fps :", fps)
 
 #프레임을 저장할 디렉토리를 생성
@@ -29,15 +25,20 @@ try:
 except OSError:
     print ('Error: Creating directory. ' +  filepath[:-4])
 
+#영상을 각 프레임마다 캡쳐 후 디렉토리에 저장
 count = 0
 
-while (video.isOpened()):
-    ret, image = video.read()
+while (video.isOpened()): # video 열려있는 동안 실행
+    retval, image = video.read() # image : 현재 프레임
+    # int(video.get(1)) == int(video.get(cv2.CAP_PROP_POS_FRAMES))
     if (int(video.get(1)) % fps == 0):  # 앞서 불러온 fps 값을 사용하여 1초마다 추출
-        cv2.imwrite(filepath[:-4] + "/frame%d.jpg" % count, image)
+        cv2.imwrite(filepath[:-4] + "/frame%d.jpg" % count, image) # 프레임 저장하기, cv2.imwrite(저장 경로, 저장 이미지)
         print('Saved frame number :', str(int(video.get(1))))
         count += 1
-
+    # 읽는 프레임 없으면 while문 나가기
+    if (retval == False):
+        break
+# 동영상 파일을 닫고 메모리 해제
 video.release()
 
 
--- send_text_to_arduino.py
+++ send_text_to_arduino.py
@@ -1,3 +1,4 @@
+# 라이브러리 임포트 하기
 import easyocr
 import cv2
 import matplotlib.pyplot as plt
@@ -10,7 +11,7 @@ THRESHOLD = 0.5
 reader = easyocr.Reader(['ko', 'en'])
 
 
-
+# 영상이 나눠진 프레임들을 리스트에 담아 반환
 def get_image_files(directory):
     image_files = []
     for filename in os.listdir(directory):
@@ -18,6 +19,7 @@ def get_image_files(directory):
         _, extension = os.path.splitext(filename)
         # 이미지 파일인지 확인
         if extension.lower() in ['.jpg', '.jpeg', '.png', '.gif', '.bmp']:
+            # 디렉토리 안의 사진을 리스트에 추가
             image_files.append(os.path.join(directory, filename))
     return image_files
 
@@ -26,37 +28,50 @@ directory_path = "./eng_ver"
 image_files_list = get_image_files(directory_path)
 
 # 아두이노와 시리얼 통신 설정
-ser = serial.Serial('COM7', 9600)  # 아두이노가 연결된 COM 포트와 속도를 설정합니다.
+ser = serial.Serial('COM7', 9600)  # 아두이노가 연결된 COM 포트와 속도를 설정
 time.sleep(2)  # 시리얼 포트가 열릴 때까지 잠시 대기
 
+
+# 프레임에서 텍스트 추출 후 아두이노로 전송
 def read(image_files_list):
 
+    # 프레임에서 추출한 번호판 텍스트 담을 리스트 생성
     r = []
 
+    # 한 프레임씩
     for img_path in image_files_list:
+        # 프레임에서 인식한 텍스트들을 하나로 합칠 변수 초기화
         str = ""
         img = cv2.imread(img_path)
 
         result = reader.readtext(img_path)
 
+        # bbox : 바운딩 박스  text : 인식한 텍스트  conf : 신뢰도
         for bbox, text, conf in result:
             if conf > THRESHOLD:
+                # 프레임에서 추출한 텍스트 str 변수에 사이에 공백 넣어서 하나의 번호판 문자열 완성
                 str = str + " " + text
+                # 인식한 텍스트에 바운딩 박스 노란색으로 치기
                 cv2.rectangle(img, pt1=(int(bbox[0][0]), int(bbox[0][1])), pt2=(int(bbox[2][0]), int(bbox[2][1])), color=(0, 255, 0), thickness=3)
-
+        # 번호판 문자열 리스트에 추가
         r.append(str)
+
+        # 이미 직전에 인식한 번호판이면 pass
         if len(r) > 1 and r[len(r) - 2] == r[len(r) - 1]:
             pass
+        # 자동차 번호판 아두이노로 전송
         else:
+            # OCR 결과를 아두이노로 전송
+            ser.write((str + '\n').encode('utf-8'))  # 텍스트를 시리얼 포트를 통해 아두이노로 전송
+
+            # 번호판 출력 + 프레임 보여주기
             print(r[int(len(r)) - 1])
             plt.figure(figsize=(8, 8))
             plt.imshow(img[:, :, ::-1])
-
-            # OCR 결과를 아두이노로 전송
-            ser.write((str + '\n').encode('utf-8'))  # 텍스트를 시리얼 포트를 통해 아두이노로 전송
-            time.sleep(1)  # 다음 데이터를 보내기 전에 잠시 대기
-
             plt.axis('off')
             plt.show()
 
+            time.sleep(4)  # 다음 데이터를 보내기 전에 잠시 대기 -> 다음 번호판까지 LED, 피엔조 울릴 여유주기 위해서
+
+# 함수 호출
 read(image_files_list)
